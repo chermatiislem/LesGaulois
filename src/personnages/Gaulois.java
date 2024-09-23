ed9081bda4248889508c98777612033d9bf1bc05
Islem Chermati <chs3464b@ensfsi.onmicrosoft.com>
2024-09-23 19:03:56 +0200

    perso

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,5 +4,47 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public void setForce(int force) {
+		this.force = force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	public void parler(String texte) {
+		String parole = prendreParole();
+		
+	}
+	private String prendreParole() {
+
+		return null;
+	}
+    public static void main(String[] args) {
+		Gaulois Asterix = new Gaulois("Asterix",8);
+		System.out.println(Asterix);
+		
+	}
+    
 
 }
