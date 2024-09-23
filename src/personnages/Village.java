f6ce440ed219f5b5886e6a044cf04351cbe03930
Islem Chermati <chs3464b@ensfsi.onmicrosoft.com>
2024-09-23 19:44:01 +0200

    perso

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -5,8 +5,9 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois =0;
-	public Village(String nom) {
+	public Village(String nom,int nbVillageoisMaximum) {
 	this.nom = nom;
+	villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	public void setChef(Chef chef) 
 	{
@@ -15,5 +16,10 @@ public class Village {
 	
 	public String getNom() {
 	return nom;
+	}
+	public void ajouterHabitant(Gaulois habitantajouter) {
+		villageois[nbVillageois]= habitantajouter;
+		nbVillageois++;
+		
 	}
 }
